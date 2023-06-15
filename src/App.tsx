5fd7873b5f85b0e5f4788207b5e965bdebdafca1
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2023-06-15 21:59:31 +0300

    Initialization of the project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="heading">Test task GNA</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
