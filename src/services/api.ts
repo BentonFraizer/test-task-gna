9d46e394448c8393962eefd62aee54e8ce0a4430
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2023-06-19 18:09:32 +0300

    feature. Change data source

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-const BACKEND_URL = 'http://localhost:3001';
+const BACKEND_URL = 'https://649030a01e6aa71680cac491.mockapi.io/';
 const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = (): AxiosInstance => {
